eec04c4bd9a55cebc4fe6d3c5589152018927c0d
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-12 11:03:55 +1000

    add book model

---
+++ models/book.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema(
+    {
+        isbn: {
+            type: Number,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        subtitle: {
+            type: String
+        },
+        author: {
+            type: String,
+            required: true
+        },
+        published: {
+            type: Date
+        },
+        publisher: {
+            type: String
+        },
+        pages: {
+            type: String
+        },
+        description: {
+            type: String
+        },
+        website: {
+            type: String
+        }
+    }
+)
+
+modules.exports = mongoose.model('book', bookSchema)
