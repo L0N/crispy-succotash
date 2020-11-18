f0076ba251ff4eb54620ebe04091e7b2796ba15c
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-19 09:33:29 +1000

    Update library.js
    include test routes

--- routes/library.js
+++ routes/library.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const book = require('../models/book')
+const axios = require('axios');
+const book = require('../models/book');
+
+//test add
+router.get('/test', (req, res) => {
+    book.insertMany(books)
+        .then((books) => {
+            console.log(books)
+        });
+})
 
 // Getting all
 router.get('/', (req, res) => {
-    res.send("Hello reader")
+
+    axios.get('https://swapi.dev/api/people/1')
+        .then((response) => {
+            const books = book.find()
+            response.send(books)
+            // console.log(response.headers);
+            // res.send(response.data);
+            console.log("Promise Resolved")
+        })
+        .catch((error) => {
+            response.status(500).json({ message: error.message })
+        })
 })
 
 // Getting one
@@ -26,5 +46,14 @@ router.patch('/:id', (req, res) => {
 router.patch('/:id', (req, res) => {
 
 })
+
+async function getBook(req, res, next) {
+    try{
+        book = await book.findById(req.params.id)
+        if (book === null) return res.status(404).json({ message: "Cannot find book"});
+    } catch (error){
+
+    }
+}
 
 module.exports = router
