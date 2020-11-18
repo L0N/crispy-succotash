edfe4a0fd67755aae2e95c7e8351679efe61dddb
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-19 09:33:33 +1000

    Update server.js
    include test routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bookModel = require('./models/book')
+const booksData = require('./data/booksData')
 require('dotenv').config();
 
 const app = express();
@@ -50,26 +52,78 @@ db.once('open', () => {console.log("Connected to DB")})
 app.use(cors())
 app.use(express.json())
 
-const libraryRoutes = require('./routes/library');
-app.use('/library', libraryRoutes)
+app.get ('/test-book', (req, res) => {
+    bookToAdd = new bookModel({
+        isbn: 9781491904244,
+        title: "You Don't Know JS",
+        author: "Kyle Simpson"
+    })
+    bookToAdd.save()
+        .then((book) => {
+            console.log('test book saved')
+            console.log(book)
+            res.send(book)
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send(err);
+        })
+} )
 
-app.get('/', (req, res) => {
-    res.send("Hey, the API is working")
+app.get('/test-seed', (req, res) => {
+    book.insertMany(booksData);
+    res.send(booksData)
 })
+// get all
+app.get('/', async (req, res) => {
+    try {
+        const books = await bookModel.find()
+        res.json(books)
+    } catch(error){
+        res.status(500).json({ message: error.message })
+    }
+})
+//get one
+app.get('/:id', getisbn, (req, res) => {
+    res.send(res.params.id)
+})
+//create one
+app.post('/', async (req, res) => {
+    const addBook = new bookModel({
+        isbn: req.body.isbn,
+        title: req.body.title,
+        subtitle: req.body.subtitle,
+        author: req.body.author,
+        published: req.body.published,
+        publisher: req.body.publisher,
+        pages: req.body.pages,
+        description: req.body.description,
+        website: req.body.website
+    })
 
-app.get('/records', (req, res) => {
-    console.log(req.query)
-    res.send(records)
+    try {
+        const newBook = await addBook.save()
+        res.status(201).json(newBook)
+    } catch(error) {
+        res.status(400).json({ message: error.message })
+    }
 })
+// update one
 
-app.post('/records', (req, res) => {
-    records.push(req.body)
-    res.send(req.body)
-})
 
-app.delete('/banana-split', (req, res) => {
-    res.send("It's arbitrary, so we didn't delete anything")
-})
+async function getisbn(req, res, next){
+    let book;
+    try{
+        book = await bookModel.findById(req.params.id);
+        if (book === null) {
+            return res.status(404).json({ message: 'book not found' });
+        }
+    }catch(error){
+        return res.status(500).json({ message: error.message });
+    }
+    res.book = book;
+    next();
+}
 
 app.listen(port, () => {
     console.log(`The API is running at ${port}`)
